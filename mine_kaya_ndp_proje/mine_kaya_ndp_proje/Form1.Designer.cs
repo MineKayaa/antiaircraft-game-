3924f20a1200a15e006ef24592c55b7631a76805
Mine KAYA <47714688+MineKayaa@users.noreply.github.com>
2020-06-09 20:29:28 +0300

    Add files via upload

---
+++ mine_kaya_ndp_proje/mine_kaya_ndp_proje/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace mine_kaya_ndp_proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mermi_zaman = new System.Windows.Forms.Timer(this.components);
+            this.dusman_zaman = new System.Windows.Forms.Timer(this.components);
+            this.ucak = new System.Windows.Forms.PictureBox();
+            this.dusman_haraket_ettir = new System.Windows.Forms.Timer(this.components);
+            this.mermi_dusman_carpisdimi = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ucak)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mermi_zaman
+            // 
+            this.mermi_zaman.Tick += new System.EventHandler(this.mermi_zaman_Tick);
+            // 
+            // dusman_zaman
+            // 
+            this.dusman_zaman.Tick += new System.EventHandler(this.dusman_zaman_Tick);
+            // 
+            // ucak
+            // 
+            this.ucak.Image = global::mine_kaya_ndp_proje.Properties.Resources.Plane_Green;
+            this.ucak.Location = new System.Drawing.Point(246, 413);
+            this.ucak.Name = "ucak";
+            this.ucak.Size = new System.Drawing.Size(80, 45);
+            this.ucak.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ucak.TabIndex = 0;
+            this.ucak.TabStop = false;
+            // 
+            // dusman_haraket_ettir
+            // 
+            this.dusman_haraket_ettir.Tick += new System.EventHandler(this.dusman_haraket_ettir_Tick);
+            // 
+            // mermi_dusman_carpisdimi
+            // 
+            this.mermi_dusman_carpisdimi.Tick += new System.EventHandler(this.mermi_dusman_carpisdimi_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(586, 63);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyunu başlatmak için ENTER tuşuna basınız.\r\nUçaksavarı haraket ettirmek için SAĞ/" +
+    "SOL YÖN TUŞLARINI kullanın.\r\nAteş etmek için BOŞLUK tuşuna basınız.\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ucak);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ucak)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ucak;
+        private System.Windows.Forms.Timer mermi_zaman;
+        private System.Windows.Forms.Timer dusman_zaman;
+        private System.Windows.Forms.Timer dusman_haraket_ettir;
+        private System.Windows.Forms.Timer mermi_dusman_carpisdimi;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ mine_kaya_ndp_proje/mine_kaya_ndp_proje/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace mine_kaya_ndp_proje
+{
+    public partial class Form1 : Form
+    {
+        bool oyun_sonlandi = false;
+
+
+        public Form1()
+        {
+            this.CenterToScreen();
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            //merminin formda hareketi
+            mermi_zaman.Enabled = true; 
+            mermi_zaman.Start();
+            mermi_zaman.Interval = 1;
+
+            //dusmanin formda random konuma eklenmesi
+            dusman_zaman.Enabled = true;
+            dusman_zaman.Stop();
+            dusman_zaman.Interval = 1200;
+            //dusmanin formda haraketi
+            dusman_haraket_ettir.Enabled = true;
+            dusman_haraket_ettir.Stop();
+            dusman_haraket_ettir.Interval = 40;
+
+            //mermi ve dusmanin formda carpismasi
+            mermi_dusman_carpisdimi.Enabled = true;
+            mermi_dusman_carpisdimi.Stop();
+            mermi_dusman_carpisdimi.Interval = 10;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //klavyeden sol tusa tiklanmasi
+            if (e.KeyCode == Keys.Left)
+            {
+                if (ucak.Left <= 0) //ucak formunun sag tarafindan cikarsa
+                    ucak.Left = 0;
+                else
+                    ucak.Left -= 20;
+            }
+
+            //klavyeden sag tusa tiklanmasi
+            else if (e.KeyCode == Keys.Right) 
+            {
+                if (ucak.Left > this.Width - (5 + ucak.Width))//ucak formunun sol tarafindan cikarsa
+                    ucak.Left = this.Width-ucak.Width-4;
+                else
+                    ucak.Left += 20;
+            }
+
+            //klavyeden bosluk tusuna tiklanmasi
+            else if(e.KeyCode==Keys.Space)
+            {
+                PictureBox mermi = new PictureBox();  //picturbox turunde mermi uretir
+                mermi.Image = Properties.Resources.bullet_in_motion;  //mermi fotografini ekler
+                mermi.SizeMode = PictureBoxSizeMode.StretchImage;  //merminin boyutunu picturebox boyuyuna uygun ayarlar
+                mermi.SetBounds(ucak.Location.X + ucak.Width/2-7, ucak.Location.Y - 20, 15, 25);  //merminin konumunu belirler
+                this.Controls.Add(mermi);  //kontrole ekler
+                mermi.Visible = true; //mermiyi gorunur yapar
+            }
+            else if(e.KeyCode==Keys.Enter)  //klavyeden entere tiklanmasi
+            {
+                if(oyun_sonlandi==false) //oyunun basladiginda
+                {
+                    dusman_zaman.Start();  
+                    dusman_haraket_ettir.Start();
+                    mermi_dusman_carpisdimi.Start();
+                }
+                if(oyun_sonlandi==true)  //eger oyun sonlanmissa
+                {
+                    //formu yeniden baslatir
+                    this.Refresh();
+                    Refresh();
+                    this.Hide();
+                    Form1 yeniden_baslat = new Form1();
+                    yeniden_baslat.Show();
+                    oyun_sonlandi = false;
+                }
+            }
+        }
+
+
+
+        private void mermi_zaman_Tick(object sender, System.EventArgs e)
+        {
+
+            //kontrol icinden mermiyi bulmak icin
+            foreach (Control mermi_bul in this.Controls)
+            {
+                if (mermi_bul.GetType() == typeof(PictureBox)) //kontrol icindeki nesnenin turu picturebox turundeyse
+                {
+                    PictureBox mermi = mermi_bul as PictureBox;
+                    if (mermi.Height == 25)  //picture boxun yuksekligi 25 ise.( 25 yuksekligi mermi olusturma kisminda yani space tiklandiginda belirlenir)
+                    {
+                        if (mermi.Location.Y > 0) //merminin formdaki konumu formun ustunden cikdiysa konumunu eksiltir
+                          mermi.Top -= 3;
+                        
+                        else //eger ciktiysa mermiyi siler
+                          mermi.Dispose();
+                        
+                    }
+                }
+            }
+        }
+
+        private void dusman_zaman_Tick(object sender, System.EventArgs e)
+        {
+            Random konum = new Random();  //random bir konum belirler
+            PictureBox dusman = new PictureBox();  //picturebox turunde bir nesne uretir
+            dusman.Image = Properties.Resources.dusman_f15;  //dusman pictureboxunun fotografini ekler
+            int locationX = konum.Next(10, this.Width - 97);  //random konum olusturur formun genisligini asmayacak sekilde
+            dusman.SizeMode = PictureBoxSizeMode.StretchImage;//dusmanin boyutunu picturebox boyuyuna uygun ayarlar
+            dusman.SetBounds(locationX, 10, 40, 40);  //dusmanin formdaki konumunu ayarlar
+            this.Controls.Add(dusman);  //kontrole ekler
+            dusman.Visible = true; //dusman pictureboxunu gorunur yapar
+        }
+
+        private void dusman_haraket_ettir_Tick(object sender, EventArgs e)
+        { 
+            foreach (Control dusman_bul in this.Controls)  //kontrol icinde dusman pictureboxunu bulur
+            {
+                if (dusman_bul.GetType() == typeof(PictureBox)) //kontrol icinde bulun dusmanin turur picturebox ise
+                {
+                    PictureBox dusman = dusman_bul as PictureBox;
+                    if (dusman.Height == 40) //picture boxun yuksekligi 40 ise.( 40 yuksekligi mermi olusturma kisminda yani dusman_zaman timeri icinde  belirlenir)
+                    {
+
+                        //dusman ile ucak carpismissa
+                        if (dusman.Location.Y >= ucak.Location.Y - 40 && dusman.Location.Y <= ucak.Location.Y + 40)
+                        {
+                            if (dusman.Location.X >= ucak.Location.X - 55 && dusman.Location.X <= ucak.Location.X + 55)
+                            {
+                                //tum timerleri durdurur ve oyunu bititrir
+                                dusman_zaman.Stop();
+                                dusman_haraket_ettir.Stop();
+                                mermi_zaman.Stop();
+                                mermi_dusman_carpisdimi.Stop();
+                                oyun_sonlandi = true;
+                                MessageBox.Show("Oyun Bitti!");
+                            }
+                        }
+
+                        //dusman ucagi formdan cikmissa 
+                        if (dusman.Location.Y >= this.Height - 95)
+                        {
+                            //tum timerleri durdurur ve oyunu bititrir
+                            dusman_zaman.Stop();
+                            dusman_haraket_ettir.Stop();
+                            mermi_zaman.Stop();
+                            mermi_dusman_carpisdimi.Stop();
+                            oyun_sonlandi = true;
+                            MessageBox.Show("Oyun Bitti!");
+                        }
+                        dusman.SetBounds(dusman.Location.X, dusman.Location.Y + 3, 40, 40); //eger bitmediyse dusman yukardan konumunu artirir
+                    }
+                }
+            }
+        }
+
+        private void mermi_dusman_carpisdimi_Tick(object sender, EventArgs e)
+        {
+            mermi_dusman_carpis();
+        }
+
+        public bool mermi_dusman_carpis()
+        {
+            List<PictureBox> mermi_listesi = new List<PictureBox>(); 
+            List<PictureBox> dusman_listesi = new List<PictureBox>();
+
+            foreach (Control mermi_ve_ucak_bul in this.Controls)  //kontrolden dusman ve mermiyi bulur
+            {
+                if (mermi_ve_ucak_bul.GetType() == typeof(PictureBox))
+                {
+                    if (mermi_ve_ucak_bul.Height == 25) //mermiyi yukseklik degerine gore bulur
+                    {
+                        mermi_listesi.Add((PictureBox)mermi_ve_ucak_bul);
+                    }
+                    else if (mermi_ve_ucak_bul.Height == 40) //ucagi yukseklik degerine gore bulur
+                    {
+                        dusman_listesi.Add((PictureBox)mermi_ve_ucak_bul);
+                    }
+                }
+            }
+            for (int i = 0; i < dusman_listesi.Count; i++)  //tum dusman ucaklari icin uygular
+            {
+                for (int j = 0; j < mermi_listesi.Count; j++) //tum mermi icin uygular
+                {
+                    if (carpisdir(dusman_listesi[i], mermi_listesi[j])) //ucak ve merminin carpisdigini kontrol eder
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool carpisdir(PictureBox dusman, PictureBox mermi)
+        {
+
+            //ucag ve merminin carpisdigini kontrol eder
+               if ((mermi.Location.Y >= dusman.Location.Y - 25 && mermi.Location.Y <= dusman.Location.Y + 35) && (mermi.Location.X >= dusman.Location.X - 20 && mermi.Location.X <= dusman.Location.X + 32))
+                {
+                
+                //carpisdiysa ucak ve dusmani formdan kaldirir
+                    mermi.Dispose();
+                    dusman.Dispose();
+
+                    return true;
+                }
+            
+          
+            return false;
+        }
+
+    }
+}
